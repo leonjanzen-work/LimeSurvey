a9a574f16aac353b5365ba258f3aa775e6479f40
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-05-10 16:57:07 +0200

    Add missing form-select

--- application/views/admin/surveymenu_entries/_form.php
+++ application/views/admin/surveymenu_entries/_form.php
@@ -33,65 +33,65 @@ Yii::app()->getController()->renderPartial(
 
 		<?php echo $form->errorSummary($model); ?>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'title'); ?>
 			<?php echo $form->textField($model, 'title', array('class' => 'selector__hasInfoBox', 'size'=>60,'maxlength'=>255)); ?>
 			<?php echo $form->error($model, 'title'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'menu_id'); ?>
-			<?php echo $form->dropDownList($model, 'menu_id', $model->getMenuIdOptions(), ['options' => ['2' => ['selected' => true]]]); ?>
+			<?php echo $form->dropDownList($model, 'menu_id', $model->getMenuIdOptions(), ['options' => ['2' => ['selected' => true]], 'class' => 'form-select']); ?>
 			<?php echo $form->error($model, 'menu_id'); ?>
 		</div>
 		
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'ordering'); ?>
 			<?php echo $form->numberField($model, 'ordering'); ?>
 			<?php echo $form->error($model, 'ordering'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'menu_description'); ?>
 			<p class="selector_infoBox alert alert-info d-none"><?=gT('This will be shown when hovering over the menu.')?></p>
 			<?php echo $form->textArea($model, 'menu_description', array('class' => 'selector__hasInfoBox', 'rows'=>6, 'cols'=>50)); ?>
 			<?php echo $form->error($model, 'menu_description'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'menu_icon'); ?>
 			<p class="selector_infoBox alert alert-info d-none"><?=gT('Use a fontawesome classname, or a link to the image.')?></p>
 			<?php echo $form->textField($model, 'menu_icon', array('class' => 'selector__hasInfoBox', 'size'=>60,'maxlength'=>255)); ?>
 			<?php echo $form->error($model, 'menu_icon'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'menu_icon_type'); ?>
-			<?php echo $form->dropDownList($model, 'menu_icon_type', $model->getMenuIconTypeOptions()); ?>
+			<?php echo $form->dropDownList($model, 'menu_icon_type', $model->getMenuIconTypeOptions(), ['class' => 'form-select']); ?>
 			<?php echo $form->error($model, 'menu_icon_type'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'menu_link'); ?>
 			<p class="selector_infoBox alert alert-warning d-none"><?=gT('If the external-option is not set, this will be appended to the current admin url.')?></p>
 			<?php echo $form->textField($model, 'menu_link', array('class' => 'selector__hasInfoBox', 'size'=>60,'maxlength'=>255)); ?>
 			<?php echo $form->error($model, 'menu_link'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'permission'); ?>
-			<?php echo $form->dropDownList($model, 'permission', array_merge( ['' => 'No restriction'], Permission::getPermissionList())); ?>
+			<?php echo $form->dropDownList($model, 'permission', array_merge( ['' => 'No restriction'], Permission::getPermissionList()), ['class' => 'form-select']); ?>
 			<?php echo $form->error($model, 'permission'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<?php echo $form->labelEx($model, 'permission_grade'); ?>
-			<?php echo $form->dropDownList($model, 'permission_grade', array_merge( ['' => 'No restriction'], Permission::getPermissionGradeList())); ?>
+			<?php echo $form->dropDownList($model, 'permission_grade', array_merge( ['' => 'No restriction'], Permission::getPermissionGradeList()), ['class' => 'form-select']); ?>
 			<?php echo $form->error($model, 'permission_grade'); ?>
 		</div>
 
-		<div class="form-group">
+		<div class="mb-3">
 			<div class="list-group">
 				<label class="list-group-item col-md-6">
 					<div class="col-md-1 text-end"><input type="checkbox" data-value="1" class="checkbox selector__dataOptionModel selector__disable_following" data-priority="6" data-option='["render","link","placeholder"]' /></div>
@@ -129,76 +129,76 @@ Yii::app()->getController()->renderPartial(
 		<!-- Start collapsed advanced options -->
 		<div class="collapse" id="collapseAdvancedOptions">
 			
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'data'); ?>
 				<?php echo $form->textArea($model, 'data', array('rows'=>6, 'cols'=>50 )); ?>
 				<?php echo $form->error($model, 'data'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'name'); ?>
 				<p class="selector_infoBox alert alert-info d-none"><?=gT('The name must be unique for all menu entries throughout the software.')?></p>
 				<?php echo $form->textField($model, 'name', array('class' => 'selector__hasInfoBox', 'size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'name'); ?>
 			</div>
 			
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'menu_title'); ?>
 				<p class="selector_infoBox alert alert-info d-none"><?=gT('This is the content of the menu link - leave blank to use the title.')?></p>
 				<?php echo $form->textField($model, 'menu_title', array('class' => 'selector__hasInfoBox', 'size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'menu_title'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'menu_class'); ?>
 				<p class="selector_infoBox alert alert-warning d-none"><?=gT('If the link should have any extra classes, please insert them here.')?></p>
 				<?php echo $form->textField($model, 'menu_class', array('class' => 'selector__hasInfoBox', 'size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'menu_class'); ?>
 			</div>
 
-            <div class="form-group">
+            <div class="mb-3">
 				<?php echo $form->labelEx($model, 'user_id'); ?>
 				<?php echo $form->dropDownList($model, 'user_id', $model->getUserIdOptions()); ?>
 				<?php echo $form->error($model, 'user_id'); ?>
             </div>
             
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'action'); ?>
 				<?php echo $form->textField($model, 'action', array('size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'action'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'template'); ?>
 				<?php echo $form->textField($model, 'template', array('size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'template'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'partial'); ?>
 				<?php echo $form->textField($model, 'partial', array('size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'partial'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'classes'); ?>
 				<?php echo $form->textField($model, 'classes', array('size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'classes'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'getdatamethod'); ?>
 				<?php echo $form->textField($model, 'getdatamethod', array('size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'getdatamethod'); ?>
 			</div>
 
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'language'); ?>
 				<?php echo $form->textField($model, 'language', array('size'=>60,'maxlength'=>255)); ?>
 				<?php echo $form->error($model, 'language'); ?>
 			</div>
 			
-			<div class="form-group">
+			<div class="mb-3">
 				<?php echo $form->labelEx($model, 'showincollapse'); ?>
 				<?php echo $form->checkbox($model, 'showincollapse'); ?>
 				<?php echo $form->error($model, 'showincollapse'); ?>
