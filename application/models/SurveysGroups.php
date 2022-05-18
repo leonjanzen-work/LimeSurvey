3395cd977823cdae464b245a899b1615890f7214
Jessica Höck <jessica.hoeck@limesurvey.org>
2022-05-18 12:24:09 +0200

    Dev: Added TypeHints and removed unused code in SurveysGroups.php

--- application/models/SurveysGroups.php
+++ application/models/SurveysGroups.php
@@ -250,9 +250,12 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
         return $dataProvider;
     }
 
+    /**
+     * Returns the parent title.
+     * @return string
+     */
     public function getParentTitle()
     {
-        // "(gsid: ".$data->parent_id.")"." ".$data->parentgroup->title,
         if (empty($this->parent_id)) {
             return "";
         } else {
@@ -280,7 +283,11 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
         return $nbSurvey > 0;
     }
 
-
+    /**
+     * Returns all parents.
+     * @param boolean $bOnlyGsid false
+     * @return array
+     */
     public function getAllParents($bOnlyGsid = false)
     {
         $aParents = array();
@@ -304,19 +311,24 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
         $sSurveySettingsUrl = App()->createUrl("admin/surveysgroups/sa/surveysettings", array("id" => $this->gsid));
         $sPermissionUrl = App()->createUrl("surveysGroupsPermission/index", array("id" => $this->gsid));
         $button = "<div class='icon-btn-row'>";
+
         if ($this->hasPermission('group', 'read')) {
             $button .= '<a class="btn btn-sm btn-default" href="' . $sEditUrl . '" role="button" data-bs-toggle="tooltip" title="' . gT('Edit survey group') . '"><i class="fa fa-pencil" aria-hidden="true"></i><span class="sr-only">' . gT('Edit survey group') . '</span></a>';
         }
+
         if ($this->hasPermission('permission', 'read')) {
             $button .= '<a class="btn btn-sm btn-default" href="' . $sPermissionUrl . '" role="button" data-bs-toggle="tooltip" title="' . gT('Permission') . '"><i class="fa fa-lock" aria-hidden="true"></i><span class="sr-only">' . gT('Permission') . '</span></a>';
         }
+
         if ($this->hasPermission('surveysettings', 'read')) {
             $button .= '<a class="btn btn-sm btn-default" href="' . $sSurveySettingsUrl . '" role="button" data-bs-toggle="tooltip" title="' . gT('Survey settings') . '"><i class="fa fa-cog" aria-hidden="true"></i><span class="sr-only">' . gT('Survey settings') . '</span></a>';
         }
+
         /* Can not delete group #1 + with survey (or move it to hasPermission function ?) */
         if ($this->gsid != 1 && !$this->hasSurveys && $this->hasPermission('group', 'delete')) {
             $button .= '<span data-bs-toggle="tooltip" title="' . gT('Delete survey group') . '"><a class="btn btn-sm btn-default" href="#" data-post-url="' . $sDeleteUrl . '" data-bs-target="#confirmation-modal" role="button" data-bs-toggle="modal" data-message="' . gT('Do you want to continue?') . '"><i class="fa fa-trash text-danger " aria-hidden="true"></i></a></span>';
         }
+
         $button .= "</div>";
         return $button;
     }
@@ -341,18 +353,28 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
         return $aSurveyList;
     }
 
+    /**
+     * Returns next order position
+     * @return int
+     */
     public function getNextOrderPosition()
     {
         $oSurveysGroups = SurveysGroups::model()->findAll();
         return count($oSurveysGroups) + 1;
     }
 
+    /**
+     * Returns the parent group options
+     * @param int $gsid
+     * @return array
+     */
     public function getParentGroupOptions($gsid = null)
     {
         $criteria = new CDbCriteria();
         if (!empty($gsid)) {
             $criteria->compare("t.gsid", '<>' . $gsid);
         }
+
         // Permission
         $criteriaPerm = self::getPermissionCriteria();
         $criteria->mergeWith($criteriaPerm, 'AND');
@@ -360,21 +382,20 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
             /* If gsid is set : be sure to add current parent */
             $criteria->compare("t.gsid", $this->parent_id, false, 'OR');
         }
+
         $oSurveysGroups = SurveysGroups::model()->findAll($criteria);
         $options = [
             '' => gT('No parent group')
         ];
 
         foreach ($oSurveysGroups as $oSurveysGroup) {
-            //$options[] = "<option value='".$oSurveymenu->id."'>".$oSurveymenu->title."</option>";
-
             $aParentsGsid = $oSurveysGroup->getAllParents(true);
 
             if (! in_array($this->gsid, $aParentsGsid)) {
                 $options['' . ($oSurveysGroup->gsid) . ''] = '(' . $oSurveysGroup->name . ') ' . $oSurveysGroup->title;
             }
         }
-        //return join('\n',$options);
+        
         return $options;
     }
 
@@ -401,20 +422,26 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
         if (!Permission::model()->hasGlobalPermission("surveys", 'read') || !Permission::model()->hasGlobalPermission("surveysgroups", 'read')) {
             /* owner of surveygroup */
             $criteriaPerm->compare('t.owner_id', Yii::app()->user->id, false);
+
             /* Simple permission on SurveysGroup inside a group */
             $criteriaPerm->mergeWith(array(
                 'join' => "LEFT JOIN {{permissions}} AS permissions ON (permissions.entity_id = t.gsid AND permissions.permission='group' AND permissions.entity='surveysgroups' AND permissions.uid='" . Yii::app()->user->id . "') ",
             ));
+
             $criteriaPerm->compare('permissions.read_p', '1', false, 'OR');
+
             /* Permission on Survey inside a group */
             $criteriaPerm->mergeWith(array(
                 'join' => "LEFT JOIN {{surveys}} AS surveys ON (surveys.gsid = t.gsid)
                         LEFT JOIN {{permissions}} AS surveypermissions ON (surveypermissions.entity_id = surveys.sid AND surveypermissions.permission='survey' AND surveypermissions.entity='survey' AND surveypermissions.uid='" . Yii::app()->user->id . "') ",
             ));
+
             $criteriaPerm->compare('surveys.owner_id', Yii::app()->user->id, false, 'OR');
             $criteriaPerm->compare('surveypermissions.read_p', '1', false, 'OR');
+
             /* default survey group is always avaliable */
             $criteriaPerm->compare('t.gsid', '1', false, 'OR');
+
             /* survey group set as avaiable */
             $criteriaPerm->compare('t.alwaysavailable', '1', false, 'OR'); // Is public
         }
@@ -514,6 +541,7 @@ class SurveysGroups extends LSActiveRecord implements PermissionInterface
             $attempts++;
             $groupNumber++;
             $groupCode = "SG" . str_pad($groupNumber, 2, '0', STR_PAD_LEFT);
+            
             // We only try a number of times, based on the record count.
             if ($attempts > $surveyGroupCount + 1) {
                 throw new \Exception("Unable to get a valid survey group code after " . ($surveyGroupCount + 1) . " attempts");
